84033000125864de7709af3057763b8ef13783af
juanom07 <juanom.07@gmail.com>
2022-09-11 21:58:53 -0300

    Adding travel design

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,9 @@ import { Wrapper } from "@googlemaps/react-wrapper";
 import { calcCrow, middlePoint } from './utils/helpers';
 import { Airport } from './interfaces/airport';
 import airplane from './assets/airplane.png';
-import { useMediaQuery } from "@mui/material";
+import { useMediaQuery, CircularProgress } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import { Flight, FiberManualRecordOutlined } from '@mui/icons-material';
 
 
 function App() {
@@ -21,6 +22,8 @@ function App() {
   
   const [valueTo, setValueTo] = useState<Airport | null>(null);
   const [inputValueTo, setInputValueTo] = useState('');
+  const [loadingFrom, setLoadingFrom] = useState(false);
+  const [loadingTo, setLoadingTo] = useState(false);
   
   const [distance, setDistance] = useState<number | null>();
   const [map, setMap] = useState<google.maps.Map>();
@@ -124,7 +127,7 @@ function App() {
   return (
     <div className="flex flex-col md:p-20 md:px-5 px-2 pt-5 md:pb-5 p-1 min-h-screen w-full md:items-start items-center">
       <h1 className="md:text-3xl text-2xl font-bold text-black">Airports distance calculator</h1>
-      <div className="flex flex-col md:flex-row md:justify-between md:mt-12 mt-4 h-full w-full bg-white rounded-xl p-1">
+      <div className="flex flex-col md:flex-row md:justify-between md:mt-12 mt-4 md:h-2/3 h-full w-full md:w-2/3 bg-white rounded-xl p-1">
         <div className="md:w-1/3 w-full text-white flex flex-col">
           <div className="rounded-xl bg-white md:p-4 p-2">
             <Autocomplete
@@ -133,14 +136,31 @@ function App() {
                 setValueFrom(newValue);
               }}
               value={valueFrom}
-              inputValue={inputValue} 
-              onInputChange={(event, newInputValue) => {
-                getAirportsOptions(newInputValue, setAirportsFrom, setInputValue)
+              inputValue={inputValue}
+              loading={loadingFrom}
+              onInputChange={async (event, newInputValue) => {
+                setLoadingFrom(true)
+                try {
+                  await getAirportsOptions(newInputValue, setAirportsFrom, setInputValue)
+                  setLoadingFrom(false)
+                } catch (error) {
+                  setLoadingFrom(false)
+                }
               }}
               id="airport-from"
               options={airportsFrom}    
               getOptionLabel={ (option: string | Airport) => `${(option as Airport).iata} - ${(option as Airport).name}`}
-              renderInput={(params) => <TextField {...params} label="From" size={`${isSmallScreen ? 'small' : 'medium'}`}/>}
+              renderInput={(params) => <TextField {...params} label="From" size={`${isSmallScreen ? 'small' : 'medium'}`} InputProps={{
+                ...params.InputProps,
+                endAdornment: (
+                  <React.Fragment>
+                    {loadingFrom ? (
+                      <CircularProgress size={20}/>
+                    ) : null}
+                    {params.InputProps.endAdornment}
+                  </React.Fragment>
+                ),
+              }}/>}
             />
             <Autocomplete
               className="md:mt-6 mt-4 w-full"
@@ -149,28 +169,60 @@ function App() {
                 setValueTo(newValue);
               }}
               inputValue={inputValueTo}
-              onInputChange={(event, newInputValue) => {
-                getAirportsOptions(newInputValue, setAirportsTo, setInputValueTo)
+              loading={loadingTo}
+              onInputChange={async (event, newInputValue) => {
+                setLoadingTo(true);
+                try {
+                  await getAirportsOptions(newInputValue, setAirportsTo, setInputValueTo)
+                  setLoadingTo(false);
+                } catch (error) {
+                  setLoadingTo(false);
+                }
               }}
               id="airport-to"
               options={airportsTo}
               getOptionLabel={ (option: string | Airport) => `${(option as Airport).iata} - ${(option as Airport).name}`}
               sx={{ width: '100%' }}
-              renderInput={(params) => <TextField {...params} label="To" size={`${isSmallScreen ? 'small' : 'medium'}`}/>}
+              renderInput={(params) => <TextField {...params} label="To" size={`${isSmallScreen ? 'small' : 'medium'}`} InputProps={{
+                ...params.InputProps,
+                endAdornment: (
+                  <React.Fragment>
+                    {loadingTo ? (
+                      <CircularProgress size={20}/>
+                    ) : null}
+                    {params.InputProps.endAdornment}
+                  </React.Fragment>
+                ),
+              }}/>}
             />
             
           </div>
-            <div className="rounded-xl md:mt-4 mt-2 bg-white p-4">
-              <div className="text-dark-blue w-full">
+            <div className="rounded-xl md:mt-4 mt-2 bg-white md:px-4 px-2">
+              <div className="text-dark-blue w-full flex flex-col">
                 <p className="mb-2 text-xl font-bold">Distance information:</p>
-                <p className="ml-4"><strong>From:</strong> {valueFrom &&(valueFrom as Airport).name}</p>
-                <p className="ml-4"><strong>To:</strong> {valueTo && (valueTo as Airport).name}</p>
-                <p className="ml-4"><strong>NMI:</strong> {distance && distance.toFixed(2)}</p>
+                <div className='flex'>
+                  <div className='h-24 md:h-48 w-1/12 flex flex-col items-baseline justify-center'>
+                    <div className='rounded-full flex ml-0.5 mb-1'>
+                      <FiberManualRecordOutlined sx={{color:'grey'}} />
+                    </div>
+                    <div className='h-full m-auto border-l-4 border-dotted border-grey'></div>
+                    <div className='pb-2  pt-1'>
+                      <Flight sx={{transform: "rotate(180deg)"}}/>
+                    </div>
+                  </div>
+                  <div className='h-24 md:h-48 flex flex-col justify-between pl-2'>
+                    <p className="md:text-md text-sm"><strong>From:</strong> {valueFrom &&(valueFrom as Airport).name}</p>
+                    <div className='flex flex-col'>
+                      <p className="md:text-md text-sm"><strong>To:</strong> {valueTo && (valueTo as Airport).name}</p>
+                      <p className="md:text-md text-sm"><strong>NMI:</strong> {distance && distance.toFixed(2)}</p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
         </div>
         
-        <div className="md:w-2/3 text-white md:ml-8 p-20 md:mt-0 mt-4 rounded-xl grow" id="map">
+        <div className="md:w-2/3 text-white md:p-20 p-8 md:mt-0 mt-1 rounded-xl grow" id="map">
         <Wrapper apiKey={"AIzaSyCXusc3Z113wp1oh98OGoYgQLwEwAoRY54"}>
           <div id="map"></div>
         </Wrapper>
